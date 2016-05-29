38cded9d4739a3436e902ce63356eac41c0fafe5
scheijan <scheijan@gmail.com>
2016-05-29 02:01:26 +0200

    fix message

--- slackwarriorPlugins.js
+++ slackwarriorPlugins.js
@@ -9,7 +9,7 @@ var GENERAL_ERROR_MESSAGES = ['I\'m sorry, but there was an internal problem, it
 var TASK_ERROR_MESSAGES = ['I\'m sorry, but I didn\'t understand that command. Please feel free to ask for `task help` at any time, if you want me to show you the available commands again.']
 var NOT_MOST_URGENT_MESSAGES = ['You have more urgent tasks though... :zipper_mouth_face:',
                                 'Looks like you should have been working on something else though... :building_construction:',
-                                'But aren\'t you running out of time for some other tasks on your list :hourglass_flowing_sand:',
+                                'But aren\'t you running out of time for some other tasks on your list? :hourglass_flowing_sand:',
                                 'But shouldn\'t you be paying attention to some other construction sites? :construction:',
                                 'The clock seems to be ticking for some other tasks on your list though... :alarm_clock:']
 

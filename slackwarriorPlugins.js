b9d01914af39a4800fc7c39e9ca3b6a978010725
scheijan <scheijan@gmail.com>
2016-06-07 01:56:43 +0200

    refactoring / comments

--- slackwarriorPlugins.js
+++ slackwarriorPlugins.js
@@ -5,92 +5,6 @@ const api = require('./slackwarriorAPI');
 const init = function (controller) {
   // * * * conversations * * * //
 
-  // specific help for the task commands
-  function helpTaskConvo(bot, message) {
-    bot.startPrivateConversation(message, (err, dm) => {
-      dm.say('All commands to work with tasks start with `task`. Right now I know the following commands:')
-      dm.say('`task help`, `task`, `task list`, `task add`, `task 23`, `task 23 done`, `task 23 start`, `task 23 stop`, `task 23 modify`, `task 23 annotate`')
-      dm.say('You\'ll have to replace `23` with the `short_id` of the task you want to adress.')
-      dm.say('You can find more information about my available commands and tasks in general at slackwarrior.scheijan.net/doc.html')
-      dm.next()
-    })
-  }
-
-  // general help, offer ways to get more specific help
-  function helpConvo(bot, message) {
-    // bot.botkit.log('helpConvo', message)
-    bot.startPrivateConversation(message, (err, dm) => {
-      dm.say('You\'re looking for help on how to use my services? I\'m glad you asked!');
-      dm.say('I\'m Slackwarrior and I\'m here to help you manage your tasks.');
-      dm.say('Luckily for me some very smart people built taskwarrior.org, a really awesome task manager, so I don\'t have to do all the hard work.');
-      dm.say('And also luckily for me some other very smart people built inthe.am, which helps you sync your tasks among different devices and access them from every brower. Convenient, right?');
-      dm.say('I can talk with inthe.am and list your tasks, `add` new ones and mark them completed as you work through the list.');
-
-      // at the end of the conversation
-      dm.on('end', () => {
-        const answer = { channel: message.user, text: 'Please tell me if you want me to help you with the `onboarding` or tap on the :computer:\nIf you\'d like to know more about working with tasks, please message me with `task help` or just tap the :notebook: now.', as_user: true }
-        bot.api.chat.postMessage(answer, (postErr, response) => {
-          if (!postErr) {
-            // add the computer reaction for the user to click on to get more help regarding the onboarding process
-            bot.addReaction(response, 'computer')
-
-            // add the computer notebook for the user to click on to get more help regarding tasks
-            bot.addReaction(response, 'notebook')
-          } else {
-            bot.botkit.log('error sending message', response, postErr);
-          }
-        })
-      })
-    })
-  }
-
-  // offer help with the onboarding process and start the tokenConvo, if the user wants that
-  function onboardingConvo(bot, message) {
-    bot.startPrivateConversation(message, (err, convo) => {
-      if (!err) {
-        convo.say('If you want me to help you managing your tasks, you\'ll first need an account at inthe.am')
-        convo.say('You can sign up with a google account and it\'s completely free! :free:')
-        convo.say('Once you have an account there I need your "token", you can find it on inthe.am/configure under "API Access"')
-        convo.say('slackwarrior.scheijan.net/apikey.png')
-        convo.ask('Do you want me to add your token to my dossier now?', [
-          {
-            pattern: bot.botkit.utterances.yes,
-            callback: (response, innerConvo) => {
-              // since no further messages are queued after this,
-              // the conversation will end naturally with status == 'completed'
-              innerConvo.next();
-              tokenConvo(bot, message);
-            },
-          },
-          {
-            pattern: bot.botkit.utterances.no,
-            callback: (response, innerConvo) => {
-              innerConvo.stop();
-            },
-          },
-          {
-            default: true,
-            callback: (response, innerConvo) => {
-              innerConvo.repeat();
-              innerConvo.next();
-            },
-          },
-        ]);
-        convo.next();
-        // at the end of the conversation
-        convo.on('end', (innerConvo) => {
-          bot.botkit.log('status', innerConvo.status)
-          if (innerConvo.status === 'stopped') {
-            bot.reply(message, 'Alright, as you wish. Just tell me once you have your token, so we can begin working on your tasks.');
-          }
-        })
-      } else {
-        bot.reply(message, messages.randomErrorMessage())
-        bot.botkit.log('error starting onboarding convo', err)
-      }
-    })
-  }
-
   // ask the user for their new token and add it to local storage
   function newTokenConvo(bot, message, u) {
     const user = u;
@@ -280,18 +194,93 @@ const init = function (controller) {
     });
   }
 
-  // * * * event listeners * * * //
+  // specific help for the task commands
+  function helpTaskConvo(bot, message) {
+    bot.startPrivateConversation(message, (err, dm) => {
+      dm.say('All commands to work with tasks start with `task`. Right now I know the following commands:')
+      dm.say('`task help`, `task`, `task list`, `task add`, `task 23`, `task 23 done`, `task 23 start`, `task 23 stop`, `task 23 modify`, `task 23 annotate`')
+      dm.say('You\'ll have to replace `23` with the `short_id` of the task you want to adress.')
+      dm.say('You can find more information about my available commands and tasks in general at slackwarrior.scheijan.net/doc.html')
+      dm.next()
+    })
+  }
 
-  // handle request for help with the task system
-  controller.hears(['^help task', '^task help'], 'direct_message,direct_mention,mention', (bot, message) => {
-    helpTaskConvo(bot, message)
-  })
+  // general help, offer ways to get more specific help
+  function helpConvo(bot, message) {
+    // bot.botkit.log('helpConvo', message)
+    bot.startPrivateConversation(message, (err, dm) => {
+      dm.say('You\'re looking for help on how to use my services? I\'m glad you asked!');
+      dm.say('I\'m Slackwarrior and I\'m here to help you manage your tasks.');
+      dm.say('Luckily for me some very smart people built taskwarrior.org, a really awesome task manager, so I don\'t have to do all the hard work.');
+      dm.say('And also luckily for me some other very smart people built inthe.am, which helps you sync your tasks among different devices and access them from every brower. Convenient, right?');
+      dm.say('I can talk with inthe.am and list your tasks, `add` new ones and mark them completed as you work through the list.');
 
-  // handle request for general help
-  controller.hears(['^help'], 'direct_message,direct_mention,mention', (bot, message) => {
-    helpConvo(bot, message)
-  })
+      // at the end of the conversation
+      dm.on('end', () => {
+        const answer = { channel: message.user, text: 'Please tell me if you want me to help you with the `onboarding` or tap on the :computer:\nIf you\'d like to know more about working with tasks, please message me with `task help` or just tap the :notebook: now.', as_user: true }
+        bot.api.chat.postMessage(answer, (postErr, response) => {
+          if (!postErr) {
+            // add the computer reaction for the user to click on to get more help regarding the onboarding process
+            bot.addReaction(response, 'computer')
 
+            // add the computer notebook for the user to click on to get more help regarding tasks
+            bot.addReaction(response, 'notebook')
+          } else {
+            bot.botkit.log('error sending message', response, postErr);
+          }
+        })
+      })
+    })
+  }
+
+  // offer help with the onboarding process and start the tokenConvo, if the user wants that
+  function onboardingConvo(bot, message) {
+    bot.startPrivateConversation(message, (err, convo) => {
+      if (!err) {
+        convo.say('If you want me to help you managing your tasks, you\'ll first need an account at inthe.am')
+        convo.say('You can sign up with a google account and it\'s completely free! :free:')
+        convo.say('Once you have an account there I need your "token", you can find it on inthe.am/configure under "API Access"')
+        convo.say('slackwarrior.scheijan.net/apikey.png')
+        convo.ask('Do you want me to add your token to my dossier now?', [
+          {
+            pattern: bot.botkit.utterances.yes,
+            callback: (response, innerConvo) => {
+              // since no further messages are queued after this,
+              // the conversation will end naturally with status == 'completed'
+              innerConvo.next();
+              tokenConvo(bot, message);
+            },
+          },
+          {
+            pattern: bot.botkit.utterances.no,
+            callback: (response, innerConvo) => {
+              innerConvo.stop();
+            },
+          },
+          {
+            default: true,
+            callback: (response, innerConvo) => {
+              innerConvo.repeat();
+              innerConvo.next();
+            },
+          },
+        ]);
+        convo.next();
+        // at the end of the conversation
+        convo.on('end', (innerConvo) => {
+          bot.botkit.log('status', innerConvo.status)
+          if (innerConvo.status === 'stopped') {
+            bot.reply(message, 'Alright, as you wish. Just tell me once you have your token, so we can begin working on your tasks.');
+          }
+        })
+      } else {
+        bot.reply(message, messages.randomErrorMessage())
+        bot.botkit.log('error starting onboarding convo', err)
+      }
+    })
+  }
+
+  // handle all task commands (but help)
   function handleTaskCommand(bot, message) {
     let text = message.text;
     const lcText = message.text.toLowerCase();
@@ -317,6 +306,18 @@ const init = function (controller) {
       bot.reply(message, messages.randomTaskErrorMessage())
     }
   }
+
+  // * * * event listeners * * * //
+
+  // handle request for help with the task system
+  controller.hears(['^help task', '^task help'], 'direct_message,direct_mention,mention', (bot, message) => {
+    helpTaskConvo(bot, message)
+  })
+
+  // handle request for general help
+  controller.hears(['^help'], 'direct_message,direct_mention,mention', (bot, message) => {
+    helpConvo(bot, message)
+  })
 
   // handle all commands that start with "task" (except for "task help") and call their handler functions
   controller.hears(['^task'], 'direct_message,direct_mention,mention', (bot, message) => {
